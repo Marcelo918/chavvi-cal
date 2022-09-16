3bd701dd8400291baf19833306a82846b77da715
Marcelo918 <111820089+Marcelo918@users.noreply.github.com>
2022-09-16 12:39:53 -0700

    Updated the format per the teacher request.
    I am not too familiar with Java and therefore I didn't know about the camelcase and the printf format. I also fixed the while loop, but I think in this case the while loop works fine. The user eventually needs to press 'q' to exit the program.

--- chavvical/src/main/java/com/chavvical/App.java
+++ chavvical/src/main/java/com/chavvical/App.java
@@ -1,10 +1,9 @@
 package com.chavvical;
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 /**
  * Author: Marcelo Villalobos Diaz
- * Date: September 10, 2022
+ * Date: September 16, 2022
  * Class: CSIS26 - FA22
  * Description: Basic calculator. The user is allowed to type anything. But, the calculator is resilient.
  *              So, it only accepts valid input. Valid input is the single character menu commands like (a,b,+,-,*,=,c,q).
@@ -15,26 +14,29 @@ public class App
 {
     public static void main( String[] args )
     {
-        DecimalFormat df = new DecimalFormat("0.000");
-        float A = 0.000f; //Upon starting the calculator, the values of A and B are initially set to 0
-        float B = 0.000f; //The display precision of A and B is 3 significant figures
+        float numberA = 0.000f; //Upon starting the calculator, the values of A and B are initially set to 0
+        float numberB = 0.000f; //The display precision of A and B is 3 significant figures
 
-        char answer_a = 'a'; //Asks the user to enter a number for A
-        char answer_b = 'b'; //Asks the user to enter a number for B
-        char answer_c = '+'; //A gets the value of A plus B. In other words, A=A+B
-        char answer_d = '-'; //A gets the value of A minus B. In other words, A=A-B 
-        char answer_e = '*'; //A gets the value of A times B. In other words, A=A*B
-        char answer_f = '/'; //A gets the value of A divided by B. In other words, A=A/B. Dividing by 0 is invalid user input
-        char answer_g = 'c'; //Clears the value of A and B. A gets the value 0 and B gets the value 0
-        char answer_h = 'q'; //Quits the app
+        char answerA = 'a'; //Asks the user to enter a number for A
+        char answerB = 'b'; //Asks the user to enter a number for B
+        char answerC = '+'; //A gets the value of A plus B. In other words, A=A+B
+        char answerD = '-'; //A gets the value of A minus B. In other words, A=A-B 
+        char answerE = '*'; //A gets the value of A times B. In other words, A=A*B
+        char answerF = '/'; //A gets the value of A divided by B. In other words, A=A/B. Dividing by 0 is invalid user input
+        char answerG = 'c'; //Clears the value of A and B. A gets the value 0 and B gets the value 0
+        char answerH = 'q'; //Quits the app
+        boolean isRunning = true; //conditional statement for the while loop
 
-        while(true){
+        while(isRunning == true){
 
             //The following print statemens are the user interface of the calculator.
             System.out.println( "---------------------------------------------------------------------" );
             System.out.println("Chavvi Cal");
             System.out.println( "---------------------------------------------------------------------" );
-            System.out.println("A = " + df.format(A) + "   " + "B = " + df.format(B));
+            System.out.print("A = ");
+            System.out.printf("%.3f    ", numberA);
+            System.out.print("B = ");
+            System.out.printf("%.3f%n", numberB);
             System.out.println( "---------------------------------------------------------------------" );
             System.out.println("a     Enter a value for A");
             System.out.println("b     Enter a value for B");
@@ -49,55 +51,54 @@ public class App
 
             System.out.print("Enter a command: ");
             Scanner user1 = new Scanner(System.in); //Scans for user input
-            //answer1 = user1.next().charAt(0);
-            String answer1 = user1.next();
-            char answer2 = answer1.charAt(0);
-            if(answer1.length() != 1){ //This if statement checks the input is only one character
+            String userAnswer = user1.next();
+            char answerCheck = userAnswer.charAt(0);
+            if(userAnswer.length() != 1){ //This if statement checks the input is only one character
                 
                 System.out.println("Invalid input. Please enter a valid command!"); //Error message
                 //answer1 = user1.next();
                 continue;
-            }else if(answer2 == answer_a){ //The following if else statements is to check if the user input entered a valid command. 
+            }else if(answerCheck == answerA){ //The following if else statements is to check if the user input entered a valid command. 
                 System.out.print("Enter a number: ");
                 if(!user1.hasNextFloat()){
                     System.out.println("Invalid input. Please enter a valid number!"); //Error message
                     continue;
                 }else if(user1.hasNextFloat()){
-                    A = user1.nextFloat();
+                    numberA = user1.nextFloat();
                     continue;
                 }
-            }else if(answer2 == answer_b){
+            }else if(answerCheck == answerB){
                 System.out.print("Enter a number: ");
                 if(!user1.hasNextFloat()){
                     System.out.println("Invalid input. Please enter a valid number!"); //Error message
                     continue;
                 }else if(user1.hasNextFloat()){
-                    B = user1.nextFloat();
+                    numberB = user1.nextFloat();
                     continue;
                 }
-            }else if(answer2 == answer_c){
-                A = A+B;
+            }else if(answerCheck == answerC){
+                numberA = numberA+numberB;
                 continue;
-            }else if(answer2 == answer_d){
-                A = A-B;
+            }else if(answerCheck == answerD){
+                numberA = numberA-numberB;
                 continue;
-            }else if(answer2 == answer_e){
-                A = A*B;
+            }else if(answerCheck == answerE){
+                numberA = numberA*numberB;
                 continue;
-            }else if(answer2 == answer_f){
-                if(A==0 && B==0){
+            }else if(answerCheck == answerF){
+                if(numberA==0 && numberB==0){
                     System.out.println("Result is undefined. The value of A and B must be greater than 0!"); //Error message
-                }else if(B==0){
+                }else if(numberB==0){
                     System.out.println("Cannot divide by 0. Change the value of B first!"); //Error message
                 }else{
-                    A= A/B;
+                    numberA= numberA/numberB;
                 }
                 continue;
-            }else if(answer2 == answer_g){
-                A=0;
-                B=0;
+            }else if(answerCheck == answerG){
+                numberA=0;
+                numberB=0;
                 continue;
-            }else if(answer2 == answer_h){
+            }else if(answerCheck == answerH){
                 System.out.println("Goodbye! See you soon!"); //Goodbye message
                 break;
             }else{
@@ -106,11 +107,8 @@ public class App
             }
 
             user1.close();
-            
 
         }
-        
-
        
     }
 }
